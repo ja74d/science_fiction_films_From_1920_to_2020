b0d6ef2bd2a6f25ce98e67bfc4b3f43b0dead720
javad pournosrat <pournosrat.javad@gmail.com>
2022-03-11 15:29:16 +0330

    create csv for eache

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
+
+csv_file = open('movies2010s.csv', 'w')
 
 source = requests.get("https://en.wikipedia.org/wiki/List_of_science_fiction_films_of_the_2010s").text
 soup = BeautifulSoup(source, 'lxml')
@@ -8,29 +11,32 @@ table = soup.find('table', class_='wikitable')
 
 # all movies
 def info():
+    writer = csv.writer(csv_file)
+    informations = writer.writerow(('movie_name', 'director', 'cast_name', 'country_name', 'subgenre_name'))
     for films in table.find_all('tr'):
         try:
             movie_name = films.find_all('td')[0].text
-            print(movie_name)
+#            print(movie_name)
 
             # movie director
             director = films.find_all('td')[1].text
-            print(director)
+#            print(director)
 
             # cast
             cast_name = films.find_all('td')[2].text
-            print(cast_name)
+#            print(cast_name)
 
             # country
             country_name = films.find_all('td')[3].text
-            print(country_name)
+#            print(country_name)
 
             # subgenre/notes
             subgenre_name = films.find_all('td')[4].text
-            print(subgenre_name)
+            writer.writerow((movie_name, director, cast_name, country_name, subgenre_name))
+#            print(subgenre_name)
 
             #print('\n')
-            print('----------------------------')
+#            print('----------------------------')
         except:
             pass
 info()
