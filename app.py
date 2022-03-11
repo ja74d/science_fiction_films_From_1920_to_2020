5c2b0824d851b3953a5dd95ce62ef236f3df40de
javad pournosrat <pournosrat.javad@gmail.com>
2022-03-11 15:40:30 +0330

    create for loop to get all years in on file

--- app.py
+++ app.py
@@ -2,41 +2,46 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-csv_file = open('movies2010s.csv', 'w')
+csv_file = open('all.csv', 'w')
 
-source = requests.get("https://en.wikipedia.org/wiki/List_of_science_fiction_films_of_the_2010s").text
-soup = BeautifulSoup(source, 'lxml')
+#source = requests.get("https://en.wikipedia.org/wiki/List_of_science_fiction_films_of_the_2010s").text
+#soup = BeautifulSoup(source, 'lxml')
 
-table = soup.find('table', class_='wikitable')
+#table = soup.find('table', class_='wikitable')
 
 # all movies
 def info():
-    writer = csv.writer(csv_file)
-    informations = writer.writerow(('movie_name', 'director', 'cast_name', 'country_name', 'subgenre_name'))
-    for films in table.find_all('tr'):
-        try:
-            movie_name = films.find_all('td')[0].text
-#            print(movie_name)
+    for year in ['1920', '1930', '1940', '1950', '1960', '1970', '1980', '1990', '2000', '2010', '2020']:
+        source = requests.get(f"https://en.wikipedia.org/wiki/List_of_science_fiction_films_of_the_{year}s").text
+        soup = BeautifulSoup(source, 'lxml')
 
-            # movie director
-            director = films.find_all('td')[1].text
-#            print(director)
+        table = soup.find('table', class_='wikitable')
+        writer = csv.writer(csv_file)
+        informations = writer.writerow(('movie_name', 'director', 'cast_name', 'country_name', 'subgenre_name'))
+        for films in table.find_all('tr'):
+            try:
+                movie_name = films.find_all('td')[0].text
+    #            print(movie_name)
 
-            # cast
-            cast_name = films.find_all('td')[2].text
-#            print(cast_name)
+                # movie director
+                director = films.find_all('td')[1].text
+    #            print(director)
 
-            # country
-            country_name = films.find_all('td')[3].text
-#            print(country_name)
+                # cast
+                cast_name = films.find_all('td')[2].text
+    #            print(cast_name)
 
-            # subgenre/notes
-            subgenre_name = films.find_all('td')[4].text
-            writer.writerow((movie_name, director, cast_name, country_name, subgenre_name))
-#            print(subgenre_name)
+                # country
+                country_name = films.find_all('td')[3].text
+    #            print(country_name)
 
-            #print('\n')
-#            print('----------------------------')
-        except:
-            pass
+                # subgenre/notes
+                subgenre_name = films.find_all('td')[4].text
+                writer.writerow((movie_name, director, cast_name, country_name, subgenre_name))
+    #            print(subgenre_name)
+
+                #print('\n')
+    #            print('----------------------------')
+            except:
+                pass
 info()
