605cddd395204a71275e74a85f1b326cb413e079
javad pournosrat <pournosrat.javad@gmail.com>
2022-03-11 14:20:18 +0330

    find all movies

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from bs4 import BeautifulSoup
 source = requests.get("https://en.wikipedia.org/wiki/List_of_science_fiction_films_of_the_2010s").text
 soup = BeautifulSoup(source, 'lxml')
 
-for films in soup.find_all('td'):
-    movie_name = films.i.text
-    print(movie_name)
+table = soup.find('table', class_='wikitable')
+
+for films in table.find_all('i'):
+    movie_name = films.text
+#    print(movie_name)
+
+for directors in table.find_all('tr'):
+    director = directors.find_all('td')[1].text
+    print(director)
